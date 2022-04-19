da8fbfa5a9f0eb94ff21a1314e395eab9485f8a8
Mareike <mareike.bosselmann@gmx.de>
2022-04-19 13:28:51 +0200

    Add Story for Search component

---
+++ src/components/Search.stories.js
@@ -0,0 +1,10 @@
+import Search from './Search'
+
+export default {
+  title: 'components/Search',
+  component: Search,
+}
+
+const Template = () => <Search />
+
+export const Default = Template.bind({})
